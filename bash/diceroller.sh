c929449260da5acd7cc42dadf598945bf264a2a4
ravroop200419878 <ravroop@gmail.com>
2020-01-29 22:44:15 -0500

    diceroller task

---
+++ bash/diceroller.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# this script rolls a six-sided die
+#
+
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
+
+# roll the dice and display the result
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+#rolling
+echo "
+Rolling...
+$((RAVROOP % 6 + 7)) rolled
+"
+
+echo "
+Rolling...
+$((RAVROOP % 6 + 8)) rolled
+"
+
+echo "
+Rolling...
+$((RAVROOP % 6 + 4)) rolled
+"
+echo "
+Rolling...
+$((RAVROOP % 6 + 3)) rolled
+"
+echo "
+Rolling...
+$((RAVROOP % 6 + 2)) rolled
+"
+echo "
+Rolling...
+$((RAVROOP % 6 + 5)) rolled
+"
+echo "
+Rolling...
+$((RAVROOP % 6 + 9)) rolled
+"
